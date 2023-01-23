e28d814702d44a337d2f48faf2f956f6c4f38aaf
Barton Mallory <tai_link@outlook.com>
2023-01-23 11:41:08 -0600

    Add project files.

---
+++ The Watch/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace The_Watch;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ The Watch/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Hosting;
+using The_Watch.Data;
+
+namespace The_Watch;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+            .UseMauiMaps()
+            .ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+		return builder.Build();
+	}
+}
